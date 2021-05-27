bfc1a0cac4107b97d7c645be3eda5e953a32d139
Michell Brito <michellbp@msn.com>
2021-05-27 16:56:46 -0400

    added try and catch

--- logic/calendly.js
+++ logic/calendly.js
@@ -3,55 +3,72 @@ module.exports = {
     require("dotenv").config();
     const axios = require("axios");
     const calendly_user = await this.getAdminUsername();
-    const response = axios.get(
-      `https://api.calendly.com/scheduled_events?user=${calendly_user}&min_start_time=${startTime}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
-        },
-      }
-    );
-    const results = await response;
-    const event = results.data.collection;
-    return event;
+    try {
+      const response = axios.get(
+        `https://api.calendly.com/scheduled_events?user=${calendly_user}&min_start_time=${startTime}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
+          },
+        }
+      );
+      const results = await response;
+      const event = results.data.collection;
+      return event;
+    }
+    catch(err) {
+      console.log("No response from calendly api");
+    }
+    
   },
   getEventInfo: async function (eventID) {
     require("dotenv").config();
     const axios = require("axios");
-    const response = axios.get(
-      `https://api.calendly.com/scheduled_events/${eventID}/invitees`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
-        },
-      }
-    );
-
-    const results = await response;
-    const invitee = results.data.collection[0];
-    const inviteeInfo = {
-      name: invitee.name,
-      email: invitee.email,
-      timeZone: invitee.timezone,
-    };
-
-    return inviteeInfo;
+    try{
+      const response = axios.get(
+        `https://api.calendly.com/scheduled_events/${eventID}/invitees`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
+          },
+        }
+      );
+  
+      const results = await response;
+      const invitee = results.data.collection[0];
+      const inviteeInfo = {
+        name: invitee.name,
+        email: invitee.email,
+        timeZone: invitee.timezone,
+      };
+  
+      return inviteeInfo;
+    }
+    catch(err){
+      console.log("No response from calendly api");
+    }
+    
   },
   getAdminUsername: async function(){
     require("dotenv").config();
     const axios = require("axios");
-    const response = axios.get(
-      `https://api.calendly.com//users/me`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
-        },
-      }
-    );
-
-    const results = await response;
-    const username = results.data.resource.uri;
-    return username
-
+    try{
+      const response = axios.get(
+        `https://api.calendly.com//users/me`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
+          },
+        }
+      );
+  
+      const results = await response;
+      const username = results.data.resource.uri;
+      return username  
+    }
+    catch(err){
+      console.log("No response from calendly api");
+    }
+    
   }
 };
--- logic/zoom.js
+++ logic/zoom.js
@@ -4,47 +4,58 @@ module.exports = {
     const moment = require("moment-timezone");
     const axios = require("axios");
     const zoom_user = await this.getAdminUsername();
-    const response = axios.get(
-      `https://api.zoom.us/v2/users/${zoom_user}/meetings?type=upcoming`,
-      {
-        headers: {
-          Authorization:
-            `Bearer ${process.env.ZOOM_API_KEY}`, //the token is a variable which holds the token
-        },
+
+    try{
+      const response = axios.get(
+        `https://api.zoom.us/v2/users/${zoom_user}/meetings?type=upcoming`,
+        {
+          headers: {
+            Authorization:
+              `Bearer ${process.env.ZOOM_API_KEY}`, //the token is a variable which holds the token
+          },
+        }
+      );
+  
+      const results = await response;
+      const events = results.data.meetings;
+      const upcomingEvents = [];
+  
+      for (var i = 0; i < events.length; i++) {
+        let event = {
+          name: events[i].topic,
+          start_time: events[i].start_time,
+          join_url: events[i].join_url,
+          eastern_time: moment(events[i].start_time).tz("America/New_York").format()
+        };
+        upcomingEvents.push(event);
       }
-    );
-
-    const results = await response;
-    const events = results.data.meetings;
-    const upcomingEvents = [];
-
-    for (var i = 0; i < events.length; i++) {
-      let event = {
-        name: events[i].topic,
-        start_time: events[i].start_time,
-        join_url: events[i].join_url,
-        eastern_time: moment(events[i].start_time).tz("America/New_York").format()
-      };
-      upcomingEvents.push(event);
+      return upcomingEvents;
     }
-    return upcomingEvents;
+    catch (err) {
+      console.log("No response from zoom api");
+    }
+    
   },
   getAdminUsername: async function(){
     require("dotenv").config();
     const axios = require("axios");
-    const response = axios.get(
-      `https://api.zoom.us/v2/users/`,
-      {
-        headers: {
-          Authorization:
-            `Bearer ${process.env.ZOOM_API_KEY}`, //the token is a variable which holds the token
-        },
-      }
-    );
-
-    const results = await response;
-    const username = results.data.users[0].id;
-    return username
-
+    try{
+      const response = axios.get(
+        `https://api.zoom.us/v2/users/`,
+        {
+          headers: {
+            Authorization:
+              `Bearer ${process.env.ZOOM_API_KEY}`, //the token is a variable which holds the token
+          },
+        }
+      );
+  
+      const results = await response;
+      const username = results.data.users[0].id;
+      return username  
+    }
+    catch(err){
+      console.log("No response from zoom api");
+    }
   }
 };
